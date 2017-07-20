f11c72b2bbb04b4d44433cbc6fefacebae7fce16
Zhenfei Yin <837204976@qq.com>
2017-07-20 16:55:53 +0800

    添加项目文件。

---
+++ Demo/源.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main() {
+	//vector<int> vec = { 1,-1,2,-5,-6,-7 };
+	//transform(vec.begin(), vec.end(), vec.begin(),
+	//	[](int i)->int {if (i < 0)return -i; else return i; });
+
+	//for (auto e : vec)
+	//	cout << e << endl;
+
+	size_t v1 = 42;
+	auto f = [v1]{return ++v1; };
+	v1 = 0;
+	auto j = f();
+	cout << j << endl;
+	system("pause");
+}
